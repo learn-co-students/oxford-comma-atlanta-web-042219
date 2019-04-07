6fa66a636433aa0be280dbf36f90ebcc213c0ebe
han xu <hanxu27@gmail.com>
2019-04-07 05:36:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ require "pry"
 
 def oxford_comma(array)
   if array.size == 1
-    return arry.join
+    return array.join
   end
   binding.pry
   last = array.pop
   array.join(",")
-
+  puts
 end
