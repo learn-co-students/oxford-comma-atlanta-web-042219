b90545410b2c72db6318bda52ba12bd9e659ea16
han xu <hanxu27@gmail.com>
2019-04-07 05:42:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 require "pry"
 
 def oxford_comma(array)
-  if array.size == 1
+  if array.size <= 2
     return array.join
   end
   
   last = array.pop
-  "#{array.join(",")} and #{last}"
+  "#{array.join(",")}, and #{last}"
 
 end
