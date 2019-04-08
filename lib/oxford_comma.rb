7f346d517a7f1c4768fd28e403c21f59e1fc1305
Andrew Allen <asallen67@gmail.com>
2019-04-07 23:09:14 -0500

    1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+	msg=""
+	for k in 0..array.size-1
+		msg+=array[k]
+		msg+= ", "    if k< array.size-1 && array.size > 2 
+		msg+=  "and " if k==array.size-2 && array.size > 2
+		msg+= " and " if k==array.size-2 && array.size<= 2
+	end
+	msg
 end
